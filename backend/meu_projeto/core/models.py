5463cecad1f3473146a34e9fb357029395247a12
rodrigoprado76 <rmantovanni@hotmail.com>
2026-05-25 22:42:52 -0300

    Fix: Configura URL correta do backend nas requisicoes

--- backend/meu_projeto/core/models.py
+++ backend/meu_projeto/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nome
